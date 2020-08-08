ba1f3f971890f025b9e563f856b47c09453d8cc4
Zachary Bryant <Zachary@admiralfeb.net>
2020-08-08 06:12:28 -0500

    feat: add UserInputComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserInputComponent } from './components/user-input/user-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserInputComponent
   ],
   imports: [
     BrowserModule,

15d77d345910a7cce031c50bb1ab62c9527f1e01
Zachary Bryant <Zachary@admiralfeb.net>
2020-08-09 19:22:39 -0500

    test: fix ui tests

--- src/app/services/location.service.spec.ts
+++ src/app/services/location.service.spec.ts
@@ -1,13 +1,26 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
+
+import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { LocationService } from './location.service';
 
 describe('LocationService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   let service: LocationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(LocationService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
